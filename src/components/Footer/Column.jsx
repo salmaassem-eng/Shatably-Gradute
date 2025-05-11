4cdf677273c406cf6c52c741ae3ad18bacc1305b
Salma Assem <salmaassem95@gmail.com>
2025-05-11 03:13:23 +0200

    added footer

---
+++ src/components/Footer/Column.jsx
@@ -0,0 +1,21 @@
+function FooterColumn  ( {Title , item1, item2, item3 , children})  {
+    return ( 
+        <div className="footer-col">
+          <h4>{Title}</h4>
+          <ul>
+            <li>
+              <a href="#">{item1}</a>
+            </li>
+            <li>
+              <a href="#">{item2}</a>
+            </li>
+            <li>
+              <a href="#">{item3}</a>
+            </li>
+            {children}
+          </ul>
+        </div>
+     );
+}
+ 
+export default FooterColumn;
