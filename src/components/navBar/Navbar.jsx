cfb0458d8bf01dc68d51b4b32e9c47c8c1557027
nour <nournoman311@gmail.com>
2025-05-15 03:24:41 +0300

    nav responsive

--- src/components/navBar/Navbar.jsx
+++ src/components/navBar/Navbar.jsx
@@ -1,34 +1,45 @@
 import './navbar.css';
+import { useState } from 'react';
 import searchicon from '../../assets/search-normal.svg';
 import card from '../../assets/cart.svg';
 import user from '../../assets/user.svg';
 import vector from '../../assets/Vector.svg';
 
 export default function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <>
-            <nav className='nav'>
-                <div className="sections-pages">
-                    <button className="pages">home</button>
-                    <button className="pages">services</button>
-                    <button className="pages">community</button>
-                    <button className="pages">Ai</button>
+        <nav className="nav">
+            <div className="side-nav" onClick={toggleMenu}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
+
+            <div className={`sections-pages ${isMenuOpen ? 'active' : ''}`}>
+                <button className="pages">home</button>
+                <button className="pages">services</button>
+                <button className="pages">community</button>
+                <button className="pages">Ai</button>
+            </div>
+            <div className="sections-logo">
+                <p>shatably</p>
+            </div>
+            <div className="sections-icons">
+                <div className="search-container">
+                    <input type="search" className="search-bar" />
+                    <img src={searchicon} className="search-icon" alt="search" />
                 </div>
-                <div className="sections-logo">
-                    <p>shatably</p>
+                <div className="icon-section">
+                    <img src={user} className="icon" alt="user" />
+                    <img src={card} className="icon" alt="cart" />
+                    <img src={vector} className="icon" alt="notification" />
                 </div>
-                <div className="sections-icons">
-                    <div class="search-container">
-                        <input type="search" className="search-bar" />
-                        <img src={searchicon} className='search-icon' />
-                    </div>
-                    <div className="icon-section">
-                        <img src={user} className='icon'/>
-                        <img src={card} className='icon'/>
-                        <img src={vector} className='icon'/>
-                    </div>
-                </div>
-            </nav>
-        </>
-    )
+            </div>
+        </nav>
+    );
 }
